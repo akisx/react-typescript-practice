fd7e6bad977841706f265999740b4e23519dc50e
Akihiko Inoue <aki.sxhn@gmail.com>
2017-11-12 16:29:46 +0900

    Apply material-ui theme to root component

--- src/index.tsx
+++ src/index.tsx
@@ -1,11 +1,19 @@
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
 import App from './App';
+import { MuiThemeProvider, createMuiTheme } from 'material-ui/styles';
 import registerServiceWorker from './registerServiceWorker';
 import './index.css';
 
-ReactDOM.render(
-  <App />,
-  document.getElementById('root') as HTMLElement
-);
+const theme = createMuiTheme();
+
+const ThemeApp = () => {
+  return (
+    <MuiThemeProvider theme={theme}>
+      <App />
+    </MuiThemeProvider>
+  );
+};
+
+ReactDOM.render(<ThemeApp />, document.getElementById('root') as HTMLElement);
 registerServiceWorker();
