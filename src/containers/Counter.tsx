7cca23693d1c25b7bd34d55da8129f5bd9a3935e
Akihiko Inoue <aki.sxhn@gmail.com>
2017-11-19 18:01:15 +0900

    Remove unused style

--- src/containers/Counter.tsx
+++ src/containers/Counter.tsx
@@ -1,7 +1,6 @@
 import * as React from 'react';
 import Typography from 'material-ui/Typography';
 import Button from 'material-ui/Button';
-import './Counter.css';
 import AddIcon from 'material-ui-icons/Add';
 import RemoveIcon from 'material-ui-icons/Remove';
 import { WithStyles, withStyles, StyleRules } from 'material-ui/styles';
