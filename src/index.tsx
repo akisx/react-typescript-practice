01115531a27687f46947ec43b3f1144e539073d9
Akihiko Inoue <aki.sxhn@gmail.com>
2017-11-19 18:22:46 +0900

    Remove unused svg

--- src/index.tsx
+++ src/index.tsx
@@ -3,7 +3,6 @@ import * as ReactDOM from 'react-dom';
 import App from './App';
 import { MuiThemeProvider, createMuiTheme } from 'material-ui/styles';
 import registerServiceWorker from './registerServiceWorker';
-import './index.css';
 
 const theme = createMuiTheme();
 
