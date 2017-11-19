0c1262f328c1603c600af5a444733edd88a44d16
Akihiko Inoue <aki.sxhn@gmail.com>
2017-11-19 19:32:52 +0900

    Change primary font-family to "Noto Sans JP"

--- src/index.tsx
+++ src/index.tsx
@@ -2,9 +2,17 @@ import * as React from 'react';
 import * as ReactDOM from 'react-dom';
 import App from './App';
 import { MuiThemeProvider, createMuiTheme } from 'material-ui/styles';
+import { TypographyOptions } from 'material-ui/styles/createTypography';
 import registerServiceWorker from './registerServiceWorker';
+import './index.css';
 
-const theme = createMuiTheme();
+const typography: TypographyOptions = {
+  fontFamily: '"Noto Sans JP",Roboto,"Helvetica Neue",Arial,sans-serif'
+};
+
+const theme = createMuiTheme({
+  typography: typography
+});
 
 const ThemeApp = () => {
   return (
